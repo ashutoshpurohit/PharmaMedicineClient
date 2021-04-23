53c755d6f3db0e654cc9df99b3f6cfe2e13bcb7c
ashutoshpurohit <purohitji.ashutosh@gmail.com>
2021-04-23 21:36:16 +0530

    ..

--- Startup.cs
+++ Startup.cs
@@ -20,7 +20,7 @@ namespace PharmaMedicineClient
 
         public IConfiguration Configuration { get; }
 
-        // This method gets called by the runtime. Use this method to add services to the container.
+        //This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
             services.AddCors(options =>
